4a706f3804867fe8a58eefcad0109e3c7b8d5410
Samuel109331 <60266765+Samuel109331@users.noreply.github.com>
2023-03-20 10:05:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+from tkinter import *
+import urllib.request
+import requests 
+import random
+from PIL import Image,ImageTk
+import os
+
+
+def search():
+    global user_input,screen,img
+    link = "https://www.iconfinder.com/search?q="+user_input.get()
+    info = requests.get(link)
+    contents = info.content
+    soup = BeautifulSoup(contents,'html5lib')
+    imgs = soup.find_all("img")
+    random_image = random.choice(imgs)
+    urllib.request.urlretrieve(random_image['src'],"gfg.png")
+    img =  ImageTk.PhotoImage(Image.open("gfg.png"))
+    screen.config(image=img)
+    # print(random_image['src'])
+def delete():
+    os.remove("gfg.png")
+    win.destroy()
+win = Tk()
+
+win.protocol("WM_DELETE_WINDOW",delete)
+user_input = Entry(win,width=40,font=("Arial",23))
+subButton = Button(win,font=("Arial",23),text="Submit",command=search)
+
+user_input.grid(row=0,column=0)
+subButton.grid(row=0,column=1)
+
+screen = Label(win)
+screen.grid(row=1,column=0)
+
+
+win.mainloop()
